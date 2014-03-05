d029e4356cac04d6a7280f90bced6ff6b7d6a27f
Dennis Li <denniszxxc@gmail.com>
2014-03-05 22:28:26 +0800

    index draft version 2

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 
 # read inputs from command (file position and names)
 
-# read dictionary file (if exist)
+# read dictionary file (if exist) , store to memory as List
 # loop all files
 	# read 1 file
 	# tokenizer into separated word
@@ -14,7 +14,7 @@
 			# (update freqency if exist)
 			# (add term if not exist)
 		# read positing list of that term (if exist) (don't read the whole posing)
-		# add to positing list
+		# add docID(fliename) to positing list
 		# sort positing list of that term
 	# // until all terms of DOC are read
 # // until all file are indexed. 
@@ -32,4 +32,10 @@
 		# 1 line of DocID for each term? 
 		# how about adding a new terms in the middle of the posting file? 
 			# add a line on the middle of the file?
+				# create another file 
+				# read one line from A , write to B
+				# until reacht the target line 
+				# write the line ,
+				# then continuos read&write till eof
 			# read the nth line of a file? how?
+				# read line by line using a loop (save the only line we need)
