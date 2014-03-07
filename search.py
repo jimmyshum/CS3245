087bceac9e103368a96d7f8869b77ad12c4b27a3
jimmyshum <jimmyshum2001@gmail.com>
2014-03-07 17:33:29 +0800

    Search 4 (Stack function implemented)

--- search.py
+++ search.py
@@ -5,11 +5,63 @@ import getopt
 def search(dictionary_file,postings_file,input_file,output_file):
 	inFile = open(input_file,'r')
 	outFile = open(output_file,'w')
-
+	stack = []
+	result = []
 	for line in inFile:
 		tokens = nltk.word_tokenize(line)
 		print tokens
-		
+
+		for i in range(0,len(tokens)):
+			s = tokens[i]
+			if s == "AND":
+				if stack:
+					if stack[len(stack)-1] == "AND":
+						result.append(stack.pop())
+
+				stack.append(s)
+
+
+			elif s == "OR":
+
+				if stack:
+					if stack[len(stack)-1] == "AND":
+						result.append(stack.pop())
+					if stack[len(stack)-1] == "OR":
+						result.append(stack.pop())
+
+				stack.append(s)
+				
+
+			elif s == "NOT":
+				stack.append(s)
+
+			elif s == "(":
+				stack.append(s)
+
+			elif s == ")":
+				s = stack.pop()
+				while s != "(":
+					result.append(s)
+					s = stack.pop()
+				
+
+			else:
+				result.append(s)
+				if stack:
+					if stack[len(stack)-1] == "NOT":
+						result.append(stack.pop())
+
+				
+
+		while len(stack) != 0:
+			result.append(stack.pop())
+
+		print stack
+		print result
+
+
+
+
 	
 
 
