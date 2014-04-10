d5a6f97c65638c4ccb71c7bffb374a5406a1b775
jimmyshum <jimmyshum2001@gmail.com>
2014-04-11 00:39:00 +0800

    retrieveXML 3

--- A4/retrieveXML.py
+++ A4/retrieveXML.py
@@ -30,9 +30,25 @@ def retrieveXML():
 	#print etree.tostring(title[0])
 	#print etree.tostring(desc[0])
 
+def retrievePatentXML():
+	fopen = urllib.urlopen('patsnap-corpus/EP0115517A1.xml')
+	xml = fopen.read()
+	parser = etree.XMLParser()
+	tree = etree.parse(StringIO.StringIO(xml),parser)
+	title = tree.xpath('//str[@name="Title"]/text()')
+	desc = tree.xpath('//str[@name="Abstract"]/text()')
+	
+	#for i in range (0,len(elements)):
+	#	print etree.tostring(elements[i])
+	titleTokens = nltk.word_tokenize(title[0])
+	descTokens = nltk.word_tokenize(desc[0])
+
+	print titleTokens
+	print descTokens
 
 def usage():
     print "usage: " + sys.argv[0]
 
-retrieveXML();
+#retrieveXML();
+retrievePatentXML();
 #search(dictionary_file,postings_file,input_file,output_file)
